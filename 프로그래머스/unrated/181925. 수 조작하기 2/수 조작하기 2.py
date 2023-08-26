fe4fdbfeb1bb37381969a9b848072ca2a3d5b206
OhJiwoo <74577768+Ohjiwoo-lab@users.noreply.github.com>
2023-08-26 19:55:45 +0900

    [unrated] Title: 수 조작하기 2, Time: 12.82 ms, Memory: 13 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.py
@@ -0,0 +1,15 @@
+def solution(numLog):
+    answer = ''
+    
+    for i in range(1, len(numLog)):
+        n = numLog[i]-numLog[i-1]
+        if n==1:
+            answer += 'w'
+        elif n==-1:
+            answer += 's'
+        elif n==10:
+            answer += 'd'
+        elif n==-10:
+            answer += 'a'
+    
+    return answer
