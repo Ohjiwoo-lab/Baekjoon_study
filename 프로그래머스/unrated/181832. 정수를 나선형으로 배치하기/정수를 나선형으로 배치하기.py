1ba2a51eb67b94f992efae799b788cd337c07a1d
OhJiwoo <74577768+Ohjiwoo-lab@users.noreply.github.com>
2023-09-18 13:36:39 +0900

    Update 정수를 나선형으로 배치하기.py

--- 프로그래머스/unrated/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.py
+++ 프로그래머스/unrated/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.py
@@ -6,9 +6,6 @@ def solution(n):
             temp.append(0)
         answer.append(temp)
     
-    row = [1,0,-1,0]
-    col = [0,1,0,-1]
-    
     i,j,num=0,0,1
     state=1
     while True:
@@ -16,55 +13,30 @@ def solution(n):
         if num==n*n:
             break
         num += 1
-        
-#         i=i+row[state]
-#         j=j+col[state]
-        
-#         if i<0 or i==n or j<0 or j==n:
-#             state=(state+1)%4
-            
-#         if answer[i+row[state]][j+col[state]] != 0:
-#             state=(state+1)%4
 
         if state==1:
-            j+=1
-            if j==n:
+            if (j+1)==n or answer[i][j+1] != 0:
                 state=2
-                j-=1
                 i+=1
-            elif answer[i][j] != 0:
-                state=2
-                j-=1
-                i+=1
+            else:
+                j+=1
         elif state==2:
-            i+=1
-            if i==n:
+            if (i+1)==n or answer[i+1][j] != 0:
                 state=3
-                i-=1
                 j-=1
-            elif answer[i][j] != 0:
-                state=3
-                i-=1
-                j-=1
+            else:
+                i+=1
         elif state==3:
-            j-=1
-            if j<0:
+            if (j-1)==-1 or answer[i][j-1] != 0:
                 state=4
-                j+=1
                 i-=1
-            elif answer[i][j] != 0:
-                state=4
-                j+=1
-                i-=1
+            else:
+                j-=1
         elif state==4:
-            i-=1
-            if i<0:
+            if (i-1)==-1 or answer[i-1][j] != 0:
                 state=1
-                i+=1
                 j+=1
-            elif answer[i][j] != 0:
-                state=1
-                i+=1
-                j+=1
+            else:
+                i-=1
     
     return answer
