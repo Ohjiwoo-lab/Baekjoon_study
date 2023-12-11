715ab413226a8178e98d5519ded1cd82bc9f6a6b
OhJiwoo <74577768+Ohjiwoo-lab@users.noreply.github.com>
2023-12-11 22:20:51 +0900

    [level unrated] Title: l로 만들기, Time: 6.38 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181834. l로 만들기/l로 만들기.py
@@ -0,0 +1,10 @@
+def solution(myString):
+    answer = ''
+    
+    for st in myString:
+        if st < "l":
+            answer+="l"
+        else:
+            answer+=st
+    
+    return answer
