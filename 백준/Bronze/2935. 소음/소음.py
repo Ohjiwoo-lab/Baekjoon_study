03bf1c289c3f111482d8abc6505f4ca79c46a6fc
OhJiwoo <74577768+Ohjiwoo-lab@users.noreply.github.com>
2024-05-12 23:42:22 +0900

    [Bronze III] Title: 소음, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2935. 소음/소음.py
@@ -0,0 +1,12 @@
+# 소음
+
+first_num = int(input())
+operation = input()
+second_num = int(input())
+
+if operation == '*':
+    print(first_num * second_num)
+elif operation == '+':
+    print(first_num + second_num)
+else:
+    print("잘못된 연산입니다.")
