0f4f3e5387ab141a9ba75ddf6763896e20255f70
OhJiwoo <74577768+Ohjiwoo-lab@users.noreply.github.com>
2024-06-15 23:16:13 +0900

    [Bronze IV] Title: 인공지능 시계, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Bronze/2530. 인공지능 시계/인공지능 시계.py
+++ 백준/Bronze/2530. 인공지능 시계/인공지능 시계.py
@@ -1,14 +1,24 @@
-a,b,c = map(int, input().split())
-d = int(input())
+a,b,c=map(int, input().split())
+d=int(input())
 
-c += d
+h=d//3600
+m=(d%3600)//60
+s=(d%3600)%60
 
-b += c//60
-c %= 60
+a+=h
+b+=m
+c+=s
 
-a += b//60
-b %= 60
+if c>=60:
+    b += c//60
+    c %= 60
+    
 
-a %= 24
+if b>=60:
+    a += b//60
+    b%=60
+
+if a>=24:
+    a %= 24
 
 print(a, b, c)
