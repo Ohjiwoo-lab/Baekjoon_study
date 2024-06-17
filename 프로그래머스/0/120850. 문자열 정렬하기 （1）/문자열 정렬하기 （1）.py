e5f32b6e4fdad20b99929228fbc84911ad276014
OhJiwoo <74577768+Ohjiwoo-lab@users.noreply.github.com>
2024-06-17 23:17:33 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.py
@@ -0,0 +1,10 @@
+def solution(my_string):
+    answer = []
+    
+    for s in my_string:
+        if s.isdigit():
+            answer.append(int(s))
+    
+    answer.sort()
+    
+    return answer
